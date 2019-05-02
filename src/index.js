2f082072f3ab982bb8868a5e4dfe0082b47cefc1
Gioianac <joy.oppliger@heig-vd.ch>
2019-05-02 16:24:48 +0200

    update leaflet

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import {
   tileLayer,
 } from 'leaflet'
 
+import zoological from './tourism_zoo_data.geojson'
+
 const map = initMap('map').setView([46.7785, 6.6412], 13)
 
 const osmCH = tileLayer('https://tile.osm.ch/switzerland/{z}/{x}/{y}.png', {
@@ -11,4 +13,11 @@ const osmCH = tileLayer('https://tile.osm.ch/switzerland/{z}/{x}/{y}.png', {
   bounds: [[45, 5], [48, 11]]
 })
 
+const zoo = geoJSON(zoological, {
+  onEachFeature:(feature, layer) => layer.bindPopup(feature.properties.name)
+})
+
+
 osmCH.addTo(map)
+
+zoo.addTo(map)
